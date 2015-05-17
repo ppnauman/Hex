0ddeff10e56c2f23cfe103a41392ab9969097484
Perttu Naumanen <perttu.naumanen@helsinki.fi>
2015-05-17 23:21:59 +0300

    lisatty luokkien runkoja

---
+++ Hex/src/main/java/hex/logiikka/Kuusikulmio.java
@@ -0,0 +1,33 @@
+
+package hex.logiikka;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+/**
+ *Luokka kuvaa pelilaudan kuusikulmiota. 
+ */
+public class Kuusikulmio {
+    
+    private int[] x;
+    private int[] y;
+    private Color vari;
+    
+    
+    public Kuusikulmio () {
+        
+    }
+    
+    public void asetaVari() {
+        
+    }
+    
+    public void haeVari() {
+        
+    }
+    
+    public void piirra () {
+        
+    }
+    
+}
