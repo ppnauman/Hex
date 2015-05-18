9482ec82719afa82307292c8bfea4af597d8f4f8
Perttu Naumanen <perttu.naumanen@helsinki.fi>
2015-05-19 00:34:10 +0300

    lautaa aloitettu

--- Hex/src/main/java/hex/logiikka/Kuusikulmio.java
+++ Hex/src/main/java/hex/logiikka/Kuusikulmio.java
@@ -8,6 +8,8 @@ import java.awt.Graphics;
  */
 public class Kuusikulmio extends Monikulmio {
 
+    
+    
     /**
      * Konstruktori luo uuden harmaan Kuusikulmion, jonka kulmien
      * sijainnit grafiikkakontekstissa on ilmaistu x- ja y-koordinaatteina.
@@ -16,8 +18,12 @@ public class Kuusikulmio extends Monikulmio {
      * @param yKoord y-koordinaattien taulukko
      */
     public Kuusikulmio(int[] xKoord, int[] yKoord) {
+        
         super(xKoord, yKoord);
+        
     }
+    
+    
 
     /**
      * Korvaa yliluokan samannimisen abstraktin metodin ja piirtää täytetyn
--- Hex/src/main/java/hex/logiikka/Pelilauta.java
+++ Hex/src/main/java/hex/logiikka/Pelilauta.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hex.logiikka;
+
+import java.awt.Graphics;
 
 /**
  *Luokka kuvaa kuusikulmioista koostuvan pelilaudan ja sillä vallitsevan pelitilanteen.
+ * 
  */
 public class Pelilauta {
     
+    private Monikulmio[][] pelilauta;
     
+    public Pelilauta() {
+           
+    }
+    
+    
+    /**
+     * Piirtää pelilaudan monikulmiot parametrina annetussa grafiikkakontekstissa.
+     * 
+     * @param graphics -grafiikkakonteksti
+     */
+    public void piirraLauta(Graphics graphics) {
+        for(int i=0; i < pelilauta.length; i++) {
+            for(int j=0; j<pelilauta[0].length; j++) {
+                pelilauta[i][j].piirra(graphics);
+            }
+        }
+    }
 }
